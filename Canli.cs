86dceb57d66150e5c678162bf023531c9fad2a85
Hilal Nur TINAS <hilalnurtinass@gmail.com>
2023-07-20 22:35:03 +0300

    Proje dosyası ekle.

---
+++ Canli.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractOrnek
+{
+    abstract class Canli
+    {
+        abstract public void Konus();
+
+        public void Tanit()
+        {
+            Console.WriteLine("bu bir abstract fonksiyondur");
+        }
+    }
+
+    class Kedi : Canli
+    {
+        public override void Konus()
+        {
+            Console.WriteLine("miaavvvvvvvv");
+        }
+    }
+
+    class Kopek : Canli
+    {
+        public override void Konus()
+        {
+            Console.WriteLine("hav hav");
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace AbstractOrnek
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Canli canli1 = new Canli(); //hata vermesinin sebebi abstract classların nesnesi olusturulmaz
+            Kedi kedi = new Kedi();
+            Kopek kopek = new Kopek();
+
+            kedi.Konus();
+            kopek.Konus();
+
+            kedi.Tanit();
+
+        }
+    }
+
+}
